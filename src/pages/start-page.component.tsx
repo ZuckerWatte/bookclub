0333dc792d8829f7ed1d2f132bcf80cd4da9c143
Mona Fenzl <mona.fenzl@web.de>
2020-08-05 14:34:41 +0200

    replace create react app home page with own component

---
+++ src/pages/start-page.component.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const StartPage = () => {
+  return (
+    <div className="start-page">
+      <h1>Book remarks</h1>
+    </div>
+  );
+};
+
+export default StartPage;
